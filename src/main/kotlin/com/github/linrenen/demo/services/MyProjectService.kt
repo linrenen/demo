ce4ddf749d83e29e937c0e4541e767ff10f9a78d
GitHub Action <action@github.com>
2023-01-25 10:43:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/linrenen/demo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/linrenen/demo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.linrenen.demo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linrenen.demo.MyBundle
 
 class MyProjectService(project: Project) {
 
